b096ada81390216055d9f336dce955bae41ab747
Provoski <provo.iyen27@gmail.com>
2023-12-07 13:50:57 -0800

    correction implemented

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 ''' 4-define_variables.py module '''
 '''define and annotating variable'''
 
--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python3
-'''9-element_length.py module'''
+#!/usr/bin/env python3
 from typing import Iterable, Sequence, List, Tuple
+'''9-element_length.py module'''
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
