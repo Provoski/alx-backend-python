e6c17e68812753f196ffbbe3b43ff7c689e3969a
Provoski <provo.iyen27@gmail.com>
2023-12-28 11:04:55 -0800

    created 1-concurrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -5,17 +5,11 @@ from typing import List
 """1-concurrent_coroutines module"""
 
 
-if __name__ == "__main__":
-    async def wait_random(max_delay: float = 10) -> float:
-        """module 1"""
-        delay = random.uniform(0, max_delay)
-        await asyncio.sleep(delay)
-        return delay
-
-    async def wait_n(n: int, max_delay: float) -> List[float]:
-        """module 2"""
-        delays: List[float] = []
-        tasks = [wait_random(max_delay) for _ in range(n)]
-        results = await asyncio.gather(*tasks)
-        delays = sorted(results)
-        return delays 
+wait_random = __import__('0-basic_async_syntax').wait_random
+async def wait_n(n: int, max_delay: float) -> List[float]:
+    """module 2"""
+    delays: List[float] = []
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    results = await asyncio.gather(*tasks)
+    delays = sorted(results)
+    return delays 
